27e57bf031b27925e41ced95ce0a3b110d0e229a
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-12-09 17:13:50 -0600

    Additional examples reworked

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-// Math.flor examples for randomness
-Math.floor(Math.random() * 5) + 1       // random number 1 - 5
-Math.floor(Math.random() * 3) + 20      //random number 20 - 22
+// Math.floor
+console.log('// Math.floor() example:')
+console.log('Random num between 1 - 5: ', Math.floor(Math.random() * 5) + 1);
+console.log('Random num between 20 and 22: ', Math.floor(Math.random() * 3) + 20);
 
-// .map()
+// Using .map()
 const array = ['ro', 'markus', 'jfsoijoi']
-
-// using .map()
 const mappedArray = array.map(function (el) {
     return el.toUpperCase();
 })
+console.log('\nArray mapped to capitalize all items: ', mappedArray)
 
-// using .forEach()
-mappedArray.forEach(function(el) {
-    console.log(el);
-})
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let x = numbers.map(function(ele) {
     return ele * ele;
 })
 
-console.log(x);
+console.log('Array of numbers squared: ', x);
+
+// Arrow Functions
+
+const add = (x, y) => {
+    return x + y;
+}
+
+console.log('\nArrow Function Ex 1: ', add)
+
+// Arrow function without parramater
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+console.log('Arrow Function Ex 2: ', rollDie)
+
+
+
+
+
+
+
+// Heads / Tails example
+
+/* let heads = 0;
+let tails = 0;
+
+for (let i = 0; i < 5; i++) {
+    let test = Math.floor(Math.random() * 2);
+    if (test === 0) {
+        heads++;
+    } else {
+        tails++;
+    }
+}
+
+console.log('Heads: ', heads);
+console.log('Tails: ', tails);
+ */
