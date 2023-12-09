d0a35e4c1a4ffcd9b37ae09a92a59d515000b23c
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-12-09 13:55:59 -0600

    Add .map() and .forEach() code examples

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-Math.floor(Math.random() * 5) + 1 // random number 1 - 5
+// Math.flor examples for randomness
+Math.floor(Math.random() * 5) + 1       // random number 1 - 5
+Math.floor(Math.random() * 3) + 20      //random number 20 - 22
 
-Math.floor(Math.random() * 3) + 20 //random number 20 - 22
+// .map()
+const array = ['ro', 'markus', 'jfsoijoi']
+
+// using .map()
+const mappedArray = array.map(function (el) {
+    return el.toUpperCase();
+})
+
+// using .forEach()
+mappedArray.forEach(function(el) {
+    console.log(el);
+})
