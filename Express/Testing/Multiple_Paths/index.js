6748cb5acaba5cdca6efc32dc110d76e5cf04f30
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-13 22:17:29 -0600

    Add /dogs path for testing

--- Express/Testing/Multiple_Paths/index.js
+++ Express/Testing/Multiple_Paths/index.js
@@ -11,6 +11,13 @@ app.get('/cats', (req, res) => {
     res.send('<h1>This is the CATTTTTTTT page</h1>')
 })
 
+/**
+ * This 'path' will execute with the /dogs in the rul
+ */
+app.get('/dogs', (req, res) => {
+    res.send('<h1>WOOOOOoooOOOOOOfffFFFFFFFf</h1>')
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000...')
 })
