1459e823f2b2d900244f3b73b264e12d14b2db43
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-13 22:27:00 -0600

    Update working example

--- Express/Testing/Multiple_Paths/index.js
+++ Express/Testing/Multiple_Paths/index.js
@@ -1,6 +1,9 @@
 const express = require('express')      // import express framework
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('<h1>This is the HomePage</h1>')
+})
 
 /**
  * This 'path' will take the /cats path and execute the contained code. In this case,
@@ -16,6 +19,14 @@ app.get('/cats', (req, res) => {
  */
 app.get('/dogs', (req, res) => {
     res.send('<h1>WOOOOOoooOOOOOOfffFFFFFFFf</h1>')
+})
+
+/**
+ * This 'path' or 'route' will cover all other paths that may be input into
+ * the URL. Make sure this is last, otherwise it will trigger before actual paths do.
+ */
+app.get('*', (req, res) => {
+    res.send('<h1>404 Error</h1>')
 })
 
 app.listen(3000, () => {
