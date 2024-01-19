15933f9456b5dbf3392965f5f1f6827c11f6844c
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-19 09:43:45 -0600

    Add CoreConcept component using spread operator

--- React/Example Projects/01-starting-project/src/App.jsx
+++ React/Example Projects/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Funamental', 'Cruicial', 'Core']
 
@@ -25,7 +25,7 @@ function Header() {
 function CoreConcept(props) {
   return (
   <li>
-    <img src={props.img} alt="..." />
+    <img src={props.image} alt="..." />
     <h3>{props.title}</h3>
     <p>{props.description}</p>
   </li>
@@ -40,15 +40,16 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-              title="Components" 
-              description="The core UI building block."
-              img={componentsImg} 
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]}
+            />           
+            <CoreConcept {...CORE_CONCEPTS[2]}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
-          </ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image} 
+            />          </ul>
         </section>
       </main>
     </div>
