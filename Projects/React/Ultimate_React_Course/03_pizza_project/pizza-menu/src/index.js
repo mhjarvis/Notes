bab7385ffd084132a3de6d715509b559dcb89e59
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-28 22:12:34 -0600

    Update index.js and add StrictMode

--- Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
+++ Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello React!</h1>
+    return <h1>Hello React!</h1>;
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
