d87a671fc3be4918dd35be211844d425bcb76212
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-28 22:21:10 -0600

    Add example for pre-v18 React render

--- Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
+++ Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello React!</h1>;
+    return <h1>Hello React!!</h1>;
 }
 
+// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
+
+// React before v18
+// React.render(<App />, document.getElementById("root"))
