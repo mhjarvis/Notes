ec9c31772dd445a47c6c42509691076aa067e69e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-29 23:01:49 -0600

    Sync

--- Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
+++ Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
@@ -4,10 +4,45 @@ import ReactDOM from "react-dom/client";
 function App() {
     return (
         <>
-            <h1>Hello React!</h1>
+            <Header />
+            <Menu />
+            <Footer />
+        </>
+    );
+}
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+    return (
+        <>
+            <h2>Our menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
             <Pizza />
         </>
     );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    // const isOpen = hour >= openHour && hour <= closeHour;
+
+    return (
+        <footer>
+            {new Date().toLocaleTimeString()} - We're currently open
+        </footer>
+    );
+
+    // if we were to write it all out without the use of Babel, we would need to write:
+    // return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
@@ -27,6 +62,8 @@ root.render(
         <App />
     </React.StrictMode>
 );
+// behind the scenes, Babel actually rewrites all components into similar javascript syntax as above
+// using functions such as createElement(), etc.
 
 // React before v18
 // React.render(<App />, document.getElementById("root"))
