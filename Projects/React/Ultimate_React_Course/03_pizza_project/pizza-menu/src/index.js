cd02c97283c45c4875637e8abf5a96f77e84eb18
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-30 16:30:03 -0600

    Update

--- Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
+++ Projects/React/Ultimate_React_Course/03_pizza_project/pizza-menu/src/index.js
@@ -32,13 +32,36 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our menu</h2>
+            <Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />
+            <Pizza
+                name="Pizza Funghi"
+                ingredients="Tomato, mushrooms"
+                price={12}
+                photoName="pizzas/funghi.jpg"
+            />
             <Pizza />
             <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            <Pizza /> 
             <Pizza />
         </main>
+    );
+}
+
+function Pizza(props) {
+    return (
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
+        </div>
     );
 }
 
@@ -57,17 +80,6 @@ function Footer() {
     // if we were to write it all out without the use of Babel, we would need to write:
     // return React.createElement("footer", null, "We're currently open!");
 }
-
-function Pizza() {
-    return (
-        <>
-            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h3>Pizza Spinaci</h3>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </>
-    );
-}
-
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
