537542e1dada59d2c32eb5a06f0a6cf68e4f00c3
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-31 10:10:38 -0600

    Add initial components

--- Projects/React/Ultimate_React_Course/Challenge-1/src/main.jsx
+++ Projects/React/Ultimate_React_Course/Challenge-1/src/main.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+function Lang(props) {
+    return (
+        <>
+        </>
+    );
+}
+
 function Container() {
     return (
         <div className="container">
+            <img src="src/assets/jonas.jpeg" />
             <h1>Jonas Schmedtmann</h1>
+            <p>
+                Full-stack web developer and teacher at Udemy. When not coding
+                oro preparing a course, I like to play board games, to cook and
+                eat, or to just enjoy the Portuguese sun at the beach.
+            </p>
+            <div>
+                <Lang />
+            </div>
         </div>
     );
 }
