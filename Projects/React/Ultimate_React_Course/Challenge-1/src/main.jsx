fd6978ae88ae0e6360300e69714983c335b6d427
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-08 10:39:12 -0600

    Ignore fast refresh

--- Projects/React/Ultimate_React_Course/Challenge-1/src/main.jsx
+++ Projects/React/Ultimate_React_Course/Challenge-1/src/main.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
