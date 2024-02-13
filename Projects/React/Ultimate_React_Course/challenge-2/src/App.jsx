ed91f7e773a9cc86e95516b95fc86b8bbb2a5855
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-13 14:33:37 -0600

    Remove project files and reinstall using Vite

---
+++ Projects/React/Ultimate_React_Course/challenge-2/src/App.jsx
@@ -0,0 +1,16 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+        </>
+    );
+}
+
+export default App;
