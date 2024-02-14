352feae8cc8e90f0c6610ba3d908f246cb3163a6
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-13 21:08:30 -0600

    Add Counter component

--- Projects/React/Ultimate_React_Course/challenge-2/src/App.jsx
+++ Projects/React/Ultimate_React_Course/challenge-2/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
     return (
         <>
+        <Counter />
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -11,6 +12,10 @@ function App() {
             </div>
         </>
     );
+}
+
+function Counter() {
+    return <div>Howdy</div>;
 }
 
 export default App;
