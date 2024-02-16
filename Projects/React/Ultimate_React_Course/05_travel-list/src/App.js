92f73d80d348f0063bae5de5cfc61ebb665d4bf6
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-15 23:09:27 -0600

    Remove unnecessary files; continue linking css and create outline git push git status ; ;

--- Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
+++ Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
@@ -1,36 +1,59 @@
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      
-
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
 function Logo() {
-  return (
-    <></>
-  )
+  return <h1>🤮 Far Away</h1>;
 }
 
 function Form() {
   return (
-    <></>
-  )
+    <div className="add-form">
+      <h3>What do you need for your 🤮 trip?</h3>
+    </div>
+  );
 }
 
 function PackingList() {
   return (
-    <></>
-  )
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❎</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <></>
-  )
+    <footer className="stats">
+      You have X items on your list, and you already packed X (X%)
+    </footer>
+  );
 }
-
-export default App;
