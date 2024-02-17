b93cda7edbab936fcf19c4fd12758e3891472250
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-16 22:36:05 -0600

    Add state for quantity of items

--- Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
+++ Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
@@ -23,6 +23,7 @@ function Logo() {
 
 function Form() {
     const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
 
     // handle submit via the from element
     function handleSubmit(e) {
@@ -33,7 +34,10 @@ function Form() {
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 🤮 trip?</h3>
-            <select>
+            <select
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
                 {/* Use Array.from and .map() to create a list of options for the select */}
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
