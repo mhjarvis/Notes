b3345a0d98597463f2ddebab3451b17ce088eddb
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-18 14:37:33 -0600

    Add counter State, input field for counter, function to handle onChange event

--- Projects/React/Ultimate_React_Course/challenge-date-counter/src/App.jsx
+++ Projects/React/Ultimate_React_Course/challenge-date-counter/src/App.jsx
@@ -13,14 +13,15 @@ export default function App() {
 }
 
 function Counter() {
-    const [step, setStep] = useState(1);
+    //const [step, setStep] = useState(1);
     const [count, setCount] = useState(0);
+    const [range, setRange] = useState(1);
 
     const date = new Date();
-    date.setDate(date.getDate() + count * step);
+    date.setDate(date.getDate() + count * range);
 
     // Step variable
-    function handlePreviousStep() {
+/*     function handlePreviousStep() {
         if (step > 1) {
             setStep(step - 1);
         }
@@ -29,29 +30,56 @@ function Counter() {
         if (step < 10) {
             setStep(step + 1);
         }
-    }
+    } */
     // Count variable
     function handleNextCount() {
-        if (count < 30) {
-            setCount(count + 1);
-        }
+        setCount(Number(count) + Number(range))
+        /* if (count < 10000000) {
+            setCount(count);
+        } */
     }
     function handlePreviousCount() {
         if (count > 0) {
-            setCount(count - 1);
+            setCount(count - range);
         }
+    }
+
+    function handleCountChange(e) {
+        console.log(e)
+        setCount(e)
     }
 
     return (
         <div>
             <div className="step">
+                {/* RANGE COUNTER */}
+                <div className="range-counter">
+                    <input
+                        type="range"
+                        step={1}
+                        min="1"
+                        max="10"
+                        value={range}
+                        onChange={(e) => setRange(e.target.value)}
+                    />
+                    <p>{range}</p>
+                </div>
+                {/*
                 <Button data="-" clicker={handlePreviousStep}></Button>
                 <p>Step: {step}</p>
-                <Button data="+" clicker={handleNextStep}></Button>
+                <Button data="+" clicker={handleNextStep}></Button> 
+                */}
             </div>
             <div className="count">
                 <Button data="-" clicker={handlePreviousCount}></Button>
-                <p>Count: {count}</p>
+                <input
+                    type="number"
+                    value={count}
+                    name="range"
+                    min="1"
+                    max="10000000"
+                    onChange={(e) => handleCountChange(e.target.value)}
+                />
                 <Button data="+" clicker={handleNextCount}></Button>
             </div>
             <div>
@@ -59,7 +87,7 @@ function Counter() {
                     <p>Today is {date.toDateString()}</p>
                 ) : (
                     <p>
-                        {step * count} days from today is {date.toDateString()}
+                        {count} days from today is {date.toDateString()}
                     </p>
                 )}
             </div>
