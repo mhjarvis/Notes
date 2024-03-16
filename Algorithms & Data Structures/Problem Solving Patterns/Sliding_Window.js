7aed08c40d36b3c9f4fad469f25d81f0e12b708e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-16 11:00:53 -0500

    Update Sliding_Window solution with refactored, O(N) time solution

--- Algorithms & Data Structures/Problem Solving Patterns/Sliding_Window.js
+++ Algorithms & Data Structures/Problem Solving Patterns/Sliding_Window.js
@@ -1,20 +1,22 @@
 // Solutions using the SLIDING WINDOWS Pattern
 
+// 1. MAX SUBARRAY SUM
 
 // Write a function that accepts an array of integers and a number called num.
 // The function should calculate the maximum sum of num consecutive elements
 // in the array.
 
+// Inefficient solution:
 
 function maxSubArraySum(arr, num) {
     // edge case
-    if (arr.length === 0) return null;
-    
-    let highestNum = 0;    // track current highest number
-    
-    for (let i = 0; i < arr.length; i++) {
+    if (arr.length < num) return null;
+
+    let highestNum = 0; // track current highest number
+
+    for (let i = 0; i < arr.length - (num - 1); i++) {
         let temp = arr.slice(i, i + num);
-        let sum = temp.reduce((a, b) => a + b, 0);
+        let sum = temp.reduce((a, b) => a + b, 0); // this causes O(N^2) time
         if (sum > highestNum) {
             highestNum = sum;
         }
@@ -25,3 +27,36 @@ function maxSubArraySum(arr, num) {
 console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
 console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
 console.log(maxSubArraySum([], 4)); // null
+console.log(maxSubArraySum([4, 2, 1, 6], 1)); // 6
+
+// The more efficient solution uses two seperate loops resulting in O(N) time:
+
+function maxSubArraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+
+    if (arr.length < num) return null;
+
+    // Add up the first `num` numbers and store in maxSum
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num]; // subtract the first element from total;
+        tempSum = tempSum + arr[i]; // add next current element to total;
+        maxSum = Math.max(maxSum, tempSum); // compare and keep the largest sum
+    }
+    return maxSum;
+}
+
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubArraySum([], 4)); // null
+console.log(maxSubArraySum([4, 2, 1, 6], 1)); // 6
+
+// As we progress through the array, we keep in view a group of numbers,
+// or a window of numbers, and each increase of i results in us subtracting
+// the first number in that window and adding the next.
