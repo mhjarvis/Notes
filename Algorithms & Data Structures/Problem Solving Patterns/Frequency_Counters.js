b87875e846c005c90f8bd46d9a70ec194c61e4b7
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-16 14:13:48 -0500

    Add solution to Are_There_Duplicates using Frequency_Counters

--- Algorithms & Data Structures/Problem Solving Patterns/Frequency_Counters.js
+++ Algorithms & Data Structures/Problem Solving Patterns/Frequency_Counters.js
@@ -112,3 +112,26 @@ var digits = num.toString().split('');
 var realDigits = digits.map(Number)
 
 */
+
+// 3. Solution for ARE THERE DUPLICATES
+
+// Implement a function called, areThereDuplicates which accepts a variable
+// number of arguments, and checks whether there are any duplicates among the
+// arguments passed in.  You can solve this using the frequency counter
+//  pattern OR the multiple pointers pattern.
+
+function areThereDuplicates(...args) {
+    const obj = {};
+    for (let arg of args) {
+        if (obj[arg]) {
+            return true;
+        } else {
+            obj[arg] = 1;
+        }
+    }
+    return false;
+}
+
+console.log(areThereDuplicates(1, 2, 3)) // false
+console.log(areThereDuplicates(1, 2, 2)) // true 
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true 
