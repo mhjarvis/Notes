15938d52f59f39f503da23f12b344c95e7efc415
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-15 23:14:34 -0500

    Add second problem: countUniqueValues

--- Algorithms & Data Structures/Problem Solving Patterns/Multiple_Pointers.js
+++ Algorithms & Data Structures/Problem Solving Patterns/Multiple_Pointers.js
@@ -32,3 +32,25 @@ function sumZero(arr) {
 console.log(sumZero([-8, -4, -3, -1, 0, 2, 3, 4, 6, 7, 8, 9, 10]))
 console.log(sumZero([-8, -4, -3, 0, 1, 2, 6, 9]))
 
+// 2. COUNT UNIQUE VALUES
+
+// Implement a function called countUniqueValues which accepts a sorted array,
+// and counts the unique values in the array. There can be negative numbers in 
+// the array, but it will always be sorted.
+
+function countUniqueValues(arr) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+countUniqueValues([1, 1, 1, 1, 1, 1, 2])    // 2
+countUniqueValues([1, 2, 3, 4, 5, 6, 7, 7, 8])  // 8
+countUniqueValues([1, 1, 2, 2, 3, 4, 5, 5, 5]) // 5
