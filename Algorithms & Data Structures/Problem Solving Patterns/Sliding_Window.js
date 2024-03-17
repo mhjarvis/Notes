a70d1c89f9918a8ff1d6b2273a70efd94a0f4c42
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-16 21:29:01 -0500

    Add solution to max-subarray

--- Algorithms & Data Structures/Problem Solving Patterns/Sliding_Window.js
+++ Algorithms & Data Structures/Problem Solving Patterns/Sliding_Window.js
@@ -60,3 +60,43 @@ console.log(maxSubArraySum([4, 2, 1, 6], 1)); // 6
 // As we progress through the array, we keep in view a group of numbers,
 // or a window of numbers, and each increase of i results in us subtracting
 // the first number in that window and adding the next.
+
+// 2. MAX SUBARRAY SUM
+
+// Given an array of integers and a number, write a function called maxSubarraySum, 
+// which finds the maximum sum of a subarray with the length of the number passed to the 
+// function.
+
+// Note that a subarray must consist of consecutive elements from the original array. 
+// In the first example below, [100, 200, 300] is a subarray of the original array, but [
+// 100, 300] is not.
+
+// Constraints:
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function maxSubarraySum(arr, num) {
+    // outlier
+    if (arr.length < num) { return null }
+    
+    // sum of fist `num` values
+    let sum = arr.slice(0, num).reduce((a, b) => a + b, 0);
+    let highest = sum;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum -= arr[i];
+        sum += arr[i + num];
+        if (sum > highest) {
+            highest = sum;
+        }
+    }
+    return highest;
+}
+
+console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubarraySum([2,3], 3)) // null
+
+// 3. 
