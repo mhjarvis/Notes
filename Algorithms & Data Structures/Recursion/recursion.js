aeb1b3a39af8f1fcb77c26675c087444ec1411a6
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-18 12:55:31 -0500

    Add Recursive_Range function & solution

--- Algorithms & Data Structures/Recursion/recursion.js
+++ Algorithms & Data Structures/Recursion/recursion.js
@@ -34,6 +34,7 @@ function factorial(num){
  console.log(factorial(7)) // 5040
  
 // 3. PRODUCT OF ARRAY
+
 // Write a function called productOfArray which takes in an array
 // of numbers and returns the product of them all.
 
@@ -44,3 +45,17 @@ function productOfArray(arr) {
 
 console.log(productOfArray([1,2,3])) // 6
 console.log(productOfArray([1,2,3,10])) // 60
+
+// 4. RECURSIVE RANGE
+
+// Write a function called recursiveRange which accepts a number
+// and adds up all the numbers from 0 to the number passed to the function 
+
+function recursiveRange(num){
+    if (num === 0) { return 0 };
+    return num + recursiveRange(num - 1);
+ }
+ 
+ // SAMPLE INPUT/OUTPUT
+ console.log(recursiveRange(6)) // 21
+ console.log(recursiveRange(10)) // 55
