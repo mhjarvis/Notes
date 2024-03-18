c372cfa9bf7a6bf2a4105edeb60d20e5468791e1
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-18 15:07:40 -0500

    Update palindrome solution

--- Algorithms & Data Structures/Recursion/recursion.js
+++ Algorithms & Data Structures/Recursion/recursion.js
@@ -80,7 +80,7 @@ console.log(fib(10)) // 55
 console.log(fib(28)) // 317811
 console.log(fib(35)) // 9227465
 
-// 5. REVERSE STRING
+// 6. REVERSE STRING
 
 // Write a recursive function called reverse which accepts
 // a string and returns a new string in reverse.
@@ -101,3 +101,25 @@ function reverse(str){
 
 console.log(reverse('awesome')) // 'emosewa'
 console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
+// OPTIMIZED SOLUTION FOR REVERSE(STR): 
+
+function reverse(str){
+	if(str.length <= 1) return str;
+	return reverse(str.slice(1)) + str[0];
+}
+
+// 7. PALINDROME SOLUTION
+console.log(isPalindrome('aa')) // true
+console.log(isPalindrome('foobar')) // false
+console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('amanaplanacanalpanama')) // true
+console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
