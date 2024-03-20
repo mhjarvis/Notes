35924d1d0c7d8f0d9507b56600305163cbf73d8d
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-19 21:57:31 -0500

    Add another recursive solution

--- Algorithms & Data Structures/Recursion/recursion.js
+++ Algorithms & Data Structures/Recursion/recursion.js
@@ -206,3 +206,13 @@ function flatten(oldArr){
 
 console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
 
+// Another Solution / approach
+function capitalizeWords (array) {
+    if (array.length === 1) {
+      return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length-1)[0].toUpperCase());
+    return res;
+   
+  }
