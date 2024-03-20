7e81607974c18a8a4d92b0ba7fabdb03fb95bfad
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-19 19:14:24 -0500

    Add alternative solution to Flatten_Array

--- Algorithms & Data Structures/Recursion/recursion.js
+++ Algorithms & Data Structures/Recursion/recursion.js
@@ -168,3 +168,17 @@ console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
 console.log(flatten([[1],[2],[3]])) // [1,2,3]
 console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+
+// ALTERNATIVE SOLUTION
+
+function flatten(oldArr){
+    var newArr = []
+        for(var i = 0; i < oldArr.length; i++){
+          if(Array.isArray(oldArr[i])){
+                newArr = newArr.concat(flatten(oldArr[i]))
+          } else {
+                newArr.push(oldArr[i])
+          }
+    } 
+    return newArr;
+  }
