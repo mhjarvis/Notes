daace97c615bb5f53ac5f364e2e688ee4932009d
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-27 22:28:47 -0500

    update

--- Projects/React/Odin Project/Odin-Classes/src/App.jsx
+++ Projects/React/Odin Project/Odin-Classes/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+// Can also be imported as: import React from 'react'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Can also be rewritten with above as: class ClassInput extends React.Component {}
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    render() {
+        return (
+            <section>
+                <h3>{this.props.name}</h3>
+                <form>
+                    <label htmlFor="task-entry">Enter a task: </label>
+                    <input type="text" name="task-entry" />
+                    <button type="submit">Submit</button>
+                </form>
+                <h4>All the tasks!</h4>
+                {/* The list of all the To-Do's, displayed */}
+                <ul></ul>
+            </section>
+        );
+    }
 }
 
-export default App
+export default App;
