8b515236cd02bc14d0027975ed9f7a66dfe2d1b5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-28 19:05:16 -0500

    Add React class to rewrite functional component

--- Projects/React/Odin Project/React-Classes/src/App.jsx
+++ Projects/React/Odin Project/React-Classes/src/App.jsx
@@ -1,11 +1,68 @@
-
-import './App.css'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <div>Hello</div>
-  )
+    return (
+        <div>hello</div>
+    )
 }
 
-export default App
+class ClassInput extends React.Component {
+    
+}
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+/* const FunctionalInput = ({ name }) => {
+    const [todos, setTodos] = useState([
+        "Just some demo tasks",
+        "As an example",
+    ]);
+    const [inputVal, setInputVal] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputVal(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTodos((todo) => [...todo, inputVal]);
+        setInputVal("");
+    };
+
+    return (
+        <section>
+            <h3>{name}</h3>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="task-entry">Enter a task: </label>
+                <input
+                    type="text"
+                    name="task-entry"
+                    value={inputVal}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <h4>All the tasks!</h4>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo}>{todo}</li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+ */
