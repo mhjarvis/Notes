3d4163973a2e461972b1c754fa92b1c54c6a8176
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-01 20:10:28 -0500

    Add first test component

--- Projects/React/Odin Project/React-Testing/src/App.test.jsx
+++ Projects/React/Odin Project/React-Testing/src/App.test.jsx
@@ -1,13 +1,11 @@
-// testing setup via:
-// https://www.robinwieruch.de/vitest-react-testing-library/
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-import { describe, it, expect } from "vitest";
-
-describe("something truthy and falsy", () => {
-    it("true to be true", () => {
-        expect(true).toBe(true);
-    });
-    it("false to be false", () => {
-        expect(false).toBe(false);
+describe("App component", () => {
+    it("renders correct heading", () => {
+        render(<App />);
+        expect(screen.getByRole("heading").textContent).toMatch(
+            /our first test/i
+        );
     });
 });
--- Projects/React/Odin Project/React-Testing/vite.config.js
+++ Projects/React/Odin Project/React-Testing/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: "./tests/setups.js",
+        setupFiles: "./tests/setup.js",
     },
 });
