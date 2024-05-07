2299e5fa32f80f8aa3dcfdeb01cc562c54a1fdbb
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-07 09:30:25 -0500

    Fix form element to add items to display

--- Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
+++ Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -21,7 +27,7 @@ function Logo() {
     return <h1>🤮 Far Away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -37,6 +43,8 @@ function Form() {
             id: Date.now(),
         };
         console.log(newItem);
+
+        onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
@@ -67,11 +75,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
