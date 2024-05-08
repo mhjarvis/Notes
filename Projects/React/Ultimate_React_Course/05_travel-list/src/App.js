b8a6b4adcc6ea92bea2fcbdeba8d1b41cdc40f51
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-07 19:48:03 -0500

    Add conditional rendering to Stats

--- Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
+++ Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
@@ -127,14 +127,26 @@ function Item({ item, onDeleteItem, onToggleItems }) {
 }
 
 function Stats({ items }) {
+    // early return in case there are no items as of yet
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some tiems to your packing list</em>
+            </p>
+        );
+
     const numItems = items.length;
     const numPacked = items.filter((item) => item.packed).length;
     const percentage = Math.round((numPacked / numItems) * 100);
 
     return (
         <footer className="stats">
-            You have {numItems} items on your list, and you already packed{" "}
-            {numPacked} ({percentage}%)
+            <em>
+                {percentage === 100
+                    ? "You got everything! Ready to go!"
+                    : `You have ${numItems} items on your list, and you already packed
+                ${numPacked} (${percentage}%)`}
+            </em>
         </footer>
     );
 }
