a28ae9e55477c49c93306df4bd9c74442ebf7947
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-07 19:42:23 -0500

    Add numPacked and percentage derived state vars

--- Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
+++ Projects/React/Ultimate_React_Course/05_travel-list/src/App.js
@@ -128,11 +128,13 @@ function Item({ item, onDeleteItem, onToggleItems }) {
 
 function Stats({ items }) {
     const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
 
     return (
         <footer className="stats">
-            You have {numItems} items on your list, and you already packed X
-            (X%)
+            You have {numItems} items on your list, and you already packed{" "}
+            {numPacked} ({percentage}%)
         </footer>
     );
 }
