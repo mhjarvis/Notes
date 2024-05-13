2af95cfaf12001ebd0a14ae0e6856c831f3ee7f7
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-13 10:58:30 -0500

    Add color change to according; update classes

--- Projects/React/Ultimate_React_Course/06_accordian/src/App.js
+++ Projects/React/Ultimate_React_Course/06_accordian/src/App.js
@@ -47,7 +47,7 @@ function AccordionItem({ num, title, text }) {
     }
 
     return (
-        <div className="item" onClick={handleToggle}>
+        <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
             <p className="number">{num < 9 ? `0${num + 1}` : { num }}</p>
             <p className="title">{title}</p>
             <p className="icon">{isOpen ? "-" : "+"}</p>
