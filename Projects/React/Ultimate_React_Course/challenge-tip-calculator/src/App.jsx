e0fc014043247d0821a1bbe78d20ac543fa41cf0
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-16 15:02:38 -0500

    Update BillInput Css

--- Projects/React/Ultimate_React_Course/challenge-tip-calculator/src/App.jsx
+++ Projects/React/Ultimate_React_Course/challenge-tip-calculator/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
     return (
         <>
             <BillInput />
+            <ServiceInput text="How did you like the service? " />
+            <ServiceInput text="How did your friend like the serivce? " />
         </>
     );
 }
@@ -13,6 +15,14 @@ function BillInput() {
         <div className="bill-input-container">
             <p>How much was the bill? </p>
             <input type="text" />
+        </div>
+    );
+}
+
+function ServiceInput({ text }) {
+    return (
+        <div className="service-input-container">
+            <p>{text}</p>
         </div>
     );
 }
