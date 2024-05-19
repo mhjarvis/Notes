652fbc7d5e00984d061078ac70098807d1ec4ced
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-18 21:06:25 -0500

    Add owe display

--- Projects/React/Ultimate_React_Course/07_eat-n-split/src/App.jsx
+++ Projects/React/Ultimate_React_Course/07_eat-n-split/src/App.jsx
@@ -48,5 +48,21 @@ function FriendsList() {
 }
 
 function Friend({ friend }) {
-    return <li>{friend.name}</li>;
+    return (
+        <li>
+            <img src={friend.image} alt={friend.name} />
+            <h3>{friend.name}</h3>
+            {friend.balance < 0 && (
+                <p className="red">
+                    You owe {friend.name} ${Math.abs(friend.balance)}
+                </p>
+            )}
+            {friend.balance > 0 && (
+                <p className="green">
+                    {friend.name} owes you ${friend.balance}
+                </p>
+            )}
+            {friend.balance === 0 && <p className="gray">You are even.</p>}
+        </li>
+    );
 }
