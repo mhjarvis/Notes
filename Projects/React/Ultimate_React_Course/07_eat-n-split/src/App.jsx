1e5385f0e3b44f5d5412044f2dc06cfcb4f5bc2a
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-20 10:56:58 -0500

    Add functionality to 'Add friend' button

--- Projects/React/Ultimate_React_Course/07_eat-n-split/src/App.jsx
+++ Projects/React/Ultimate_React_Course/07_eat-n-split/src/App.jsx
@@ -25,13 +25,29 @@ const initialFriends = [
     },
 ];
 
+function Button({ children, onClick }) {
+    return (
+        <button className="button" onClick={onClick}>
+            {children}
+        </button>
+    );
+}
+
 export default function App() {
+    const [showAddFriend, setShowAddFriend] = useState(false);
+
+    function handleShowAddFriend() {
+        setShowAddFriend((show) => !show);
+    }
+
     return (
         <div className="app">
             <div className="sidebar">
                 <FriendsList />
-                <FormAddFriend />
-                <Button>Add friend</Button>
+                {showAddFriend && <FormAddFriend />}
+                <Button onClick={handleShowAddFriend}>
+                    {showAddFriend ? "Close" : "Add friend"}
+                </Button>
             </div>
             <FormSplitBill />
         </div>
@@ -69,10 +85,6 @@ function Friend({ friend }) {
             <Button>Select</Button>
         </li>
     );
-}
-
-function Button({ children }) {
-    return <button className="button">{children}</button>;
 }
 
 function FormAddFriend() {
