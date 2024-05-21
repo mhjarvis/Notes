6ea778f80f06309dbef0d7f6ab6c345bd29b77ca
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-20 19:40:11 -0500

    Break down NavBar into micro components

--- Projects/React/Ultimate_React_Course/08_use_popcorn/src/App.jsx
+++ Projects/React/Ultimate_React_Course/08_use_popcorn/src/App.jsx
@@ -46,28 +46,54 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-function NavBar() {
-    const [query, setQuery] = useState("");
+/***************************************************
+ ********************* NAVBAR **********************
+ ***************************************************/
 
+function NavBar() {
     return (
         <nav className="nav-bar">
-            <div className="logo">
-                <span role="img">🍿</span>
-                <h1>usePopcorn</h1>
-            </div>
-            <input
-                className="search"
-                type="text"
-                placeholder="Search movies..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <p className="num-results">
-                Found <strong>x</strong> results
-            </p>
+            <Logo />
+            <Search />
+            <NumResults />
         </nav>
     );
 }
+
+function Logo() {
+    return (
+        <div className="logo">
+            <span role="img">🍿</span>
+            <h1>usePopcorn</h1>
+        </div>
+    );
+}
+
+function Search() {
+    const [query, setQuery] = useState("");
+
+    return (
+        <input
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
+    );
+}
+
+function NumResults() {
+    return (
+        <p className="num-results">
+            Found <strong>x</strong> results
+        </p>
+    );
+}
+
+/***************************************************
+ ********************** MAIN ***********************
+ ***************************************************/
 
 function Main() {
     const [movies, setMovies] = useState(tempMovieData);
