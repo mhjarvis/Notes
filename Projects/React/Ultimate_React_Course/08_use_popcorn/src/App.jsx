cdc3e7f15413f1f22e2e756c82a27f6b0a46c2e1
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-05-21 20:48:48 -0500

    Breakdown into individual Movie component

--- Projects/React/Ultimate_React_Course/08_use_popcorn/src/App.jsx
+++ Projects/React/Ultimate_React_Course/08_use_popcorn/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 
@@ -126,18 +127,24 @@ function MovieList() {
     return (
         <ul className="list">
             {movies?.map((movie) => (
-                <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                        <p>
-                            <span>🗓</span>
-                            <span>{movie.Year}</span>
-                        </p>
-                    </div>
-                </li>
+                <Movie movie={movie} key={movie.imdbID} />
             ))}
         </ul>
+    );
+}
+
+function Movie({ movie }) {
+    return (
+        <li>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>🗓</span>
+                    <span>{movie.Year}</span>
+                </p>
+            </div>
+        </li>
     );
 }
 
